ad6871936f9e5c4d578439d8ebe708e51b8b96b4
Fakeru <cosmin.zeic@gmail.com>
2020-02-23 14:07:03 +0200

    [IMP] Initial Setup

---
+++ src/InputData.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputData {
+    public void input(){
+        System.out.println("Enter a and b");
+
+        Scanner
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        InputData input = new InputData();
+
+        input.input();
+    }
+}
